0faf3643d320cb83013b6715da6c48259f3f4edd
Pascal Sommer <p@pascalsommer.ch>
2017-01-04 08:58:51 +0100

    idk lol

--- canvas.js
+++ canvas.js
@@ -1,19 +1,51 @@
+function Point(x, y){
+	this.x = x;
+	this.y = y;
+	this.length = function(){
+		return Math.sqrt(this.x*this.x + this.y*this.y);
+	}
+}
 
-var Interval;
+function diff(a, b){
+	return new Point(b.x-a.x, b.y-a.y);
+}
+
+function lerp(a, b, frac){
+	return a*(1-frac) + b*frac;
+}
+
+function lerpPoint(a, b, frac){ // expects point objects
+	return new Point(lerp(a.x, b.x, frac), lerp(a.y, b.y, frac));
+}
+
+function drawPixel(ctx, pt){
+	ctx.fillRect(pt.x, pt.y, 1, 1);
+}
+
+function sandline(ctx, a, b){
+	var res = diff(a, b).length() / 10;
+	for(var i = 0; i < res; ++i){
+		var frac = Math.random();
+		drawpoint = lerpPoint(a, b, frac);
+		drawPixel(ctx, drawpoint);
+	}
+}
+
 
 function main(){
 	var c = document.getElementById('canvas');
 	if (c.getContext){
 		var ctx = c.getContext("2d");
-		ctx.lineWidth = 1;
-		ctx.globalAlpha = 0.1;
-		ctx.globalCompositeOperation = "souce-over";
+		
+
 
 		window.addEventListener('resize', resizeCanvas, false);
 
+		var interval;
+
 	    function resizeCanvas() {
-	    	if(Interval){
-	    		clearInterval(Interval);
+	    	if(interval){
+	    		clearInterval(interval);
 	    	}
 	    	
 	    	w = window.innerWidth;
@@ -21,7 +53,16 @@ function main(){
 	        c.width = w;
 	        c.height = h;
 
-		    drawStuff(ctx, w, h); 
+	        ctx.lineWidth = 1;
+			ctx.globalCompositeOperation = "souce-over";
+			ctx.fillStyle = "black";
+			ctx.strokeStyle = "rgba(10, 10, 10, 0.2)";
+	        ctx.globalAlpha = 0.2;
+
+	        A = new Point(w/3, h/2);
+	        B = new Point(w/3*2, h/2);
+
+		    interval = window.setInterval(draw, 10, ctx, w, h);
 	    }
 	    resizeCanvas();
 
@@ -30,38 +71,17 @@ function main(){
 	}
 }
 
-var PX, PY;
+var A, B;
 
-function drawStuff(ctx, width, height){
-	ctx.fillStyle = "#222";
-	ctx.strokeStyle = "rgba(10, 10, 10, 0.2)";
-
-	PX = width/2;
-	PY = height/2;
-
-	Interval = window.setInterval(walk, 1, ctx, width, height);
-	/*while(true){
-		window.setTimeout(walk, 1, ctx, width, height);
-	}*/
-
+function draw(ctx, width, height){
+	sandline(ctx, A, B);
+	
+	A.x += Math.random()*2-1; //  -.-
+	A.y += Math.random()*2-Math.random()*2; //  /.\
+	B.x += Math.random()*2-0.9; // -.--
+	B.y += Math.random()*2-0.9; // -.--
 }
 
-
-
-function walk(ctx, width, height){
-	var range = 6;
-
-	var dx = Math.random()*range-range/2;
-	var dy = Math.random()*range-range/2;
-
-	ctx.beginPath();
-	ctx.moveTo(PX, PY);
-	ctx.lineTo(PX+dx, PY+dy);
-	ctx.stroke();
-
-	PX = (PX+dx+width) % width;
-	PY = (PY+dy+height) % height;
-}
 
 function press(ev){
 	var kc = ev.keyCode;
