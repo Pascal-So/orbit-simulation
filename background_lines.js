d10e78e77dffaad55fc2885354abe26d6eb40115
Pascal Sommer <p@pascalsommer.ch>
2017-01-13 20:49:37 +0100

    change background lines back to straight lines

--- background_lines.js
+++ background_lines.js
@@ -4,16 +4,24 @@ function background_lines(ctx, width, height){
 	var y_step = x_step;
 
 	for(var x = Math.random()*x_step; x < width; x += x_step){
-		var a = new Point(x, 0);
-		var b = new Point(x, height);
 
-		sandline(ctx, a, b, height);
+		var start = Math.random()*height;
+		var end = Math.random()*height;
+
+		var a = new Point(x, start);
+		var b = new Point(x, end);
+
+		sandline(ctx, a, b, Math.abs(end-start)*2);
 	}
 	for(var y = Math.random()*y_step; y < height; y+= y_step){
-		var a = new Point(0, y);
-		var b = new Point(width, y);
 
-		sandline(ctx, a, b, width);
+		var start = Math.random()*width;
+		var end = Math.random()*width;
+		
+		var a = new Point(start, y);
+		var b = new Point(end, y);
+
+		sandline(ctx, a, b, Math.abs(end-start)*4);
 	}
 
 }
